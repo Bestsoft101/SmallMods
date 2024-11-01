303d8d58ce8d7f4bbc464332bd89f34a527a64fb
bestsoft100 <163331759+Bestsoft101@users.noreply.github.com>
2024-11-01 14:56:42 +0100

    Did some coding

---
+++ src/client/java/b100/miningmod/MiningMod.java
@@ -0,0 +1,47 @@
+package b100.miningmod;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.effect.StatusEffectUtil;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class MiningMod {
+	
+	public static final boolean INDEV = FabricLoader.getInstance().isDevelopmentEnvironment();
+	
+	public static int getBlockHitDelay(PlayerEntity player, float delta) {
+		int newBlockHitDelay;
+		if(delta >= 1.0f) {
+			newBlockHitDelay = 1;
+		}else if(delta > 1.0f / 2.0f) {
+			newBlockHitDelay = 2;
+		}else if(delta > 1.0f / 2.5f) {
+			newBlockHitDelay = 3;
+		}else if(delta > 1.0f / 3.0f) {
+			newBlockHitDelay = 4;
+		}else {
+			newBlockHitDelay = 5;
+		}
+		
+		int hasteLevel = getHasteLevel(player);
+		newBlockHitDelay -= hasteLevel;
+		newBlockHitDelay = Math.max(0, newBlockHitDelay);
+		
+		if(INDEV) {
+			print("Delta: " + (int)(delta * 100) + " Haste: " + hasteLevel + " Delay: " + newBlockHitDelay);
+		}
+		
+		return newBlockHitDelay;
+	}
+	
+	public static int getHasteLevel(PlayerEntity player) {
+		if(StatusEffectUtil.hasHaste(player)) {
+			return StatusEffectUtil.getHasteAmplifier(player) + 1;
+		}
+		return 0;
+	}
+	
+	public static void print(String string) {
+		System.out.print(string + "\n");
+	}
+	
+}
---
+++ src/client/java/b100/miningmod/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,46 @@
+package b100.miningmod.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import b100.miningmod.MiningMod;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class ClientPlayerInteractionManagerMixin {
+	
+	@Shadow
+	private MinecraftClient client;
+	
+	@Shadow
+	private int blockBreakingCooldown;
+	
+	private BlockState blockBeforeBroken;
+	
+	@Inject(method = "method_41930", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;breakBlock(Lnet/minecraft/util/math/BlockPos;)Z"))
+	private void miningmod_setInstantMineDelay(CallbackInfoReturnable<Boolean> info) {
+		// Set block break cooldown after block has been instantmined
+		blockBreakingCooldown = MiningMod.getBlockHitDelay(client.player, 100.0f);
+	}
+	
+	@Inject(method = "updateBlockBreakingProgress", at = @At(value = "HEAD"))
+	private void miningmod_getBlockStateBeforeBroken(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> ci) {
+		blockBeforeBroken = client.world.getBlockState(pos);
+	}
+
+	@ModifyConstant(method = "updateBlockBreakingProgress", constant = @Constant(intValue = 5, ordinal = 1))
+	private int miningmod_changeBlockHitDelay(int value, BlockPos pos, Direction direction) {
+		float delta = blockBeforeBroken.calcBlockBreakingDelta(this.client.player, this.client.world, pos);
+		return MiningMod.getBlockHitDelay(client.player, delta);
+	}
+	
+}
