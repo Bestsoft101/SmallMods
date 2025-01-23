35608013ca9a395e5ef8d178d7d6b50e9e71a26d
bestsoft100 <163331759+Bestsoft101@users.noreply.github.com>
2025-01-23 11:53:38 +0100

    Update to 1.21.2

--- src/main/java/b100/nofireworkboosting/mixin/FireworkRocketItemMixin.java
+++ src/main/java/b100/nofireworkboosting/mixin/FireworkRocketItemMixin.java
@@ -7,18 +7,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FireworkRocketItem;
-import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 @Mixin(value = FireworkRocketItem.class)
 public class FireworkRocketItemMixin {
 	
 	@Inject(method = "use", at = @At("HEAD"), cancellable = true)
-	private void init(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> ci) {
-		if(user.isFallFlying()) {
-			ci.setReturnValue(TypedActionResult.fail(user.getStackInHand(hand)));
+	private void init(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> ci) {
+		if(user.isGliding()) {
+			ci.setReturnValue(ActionResult.FAIL);
 		}
 	}
 	
