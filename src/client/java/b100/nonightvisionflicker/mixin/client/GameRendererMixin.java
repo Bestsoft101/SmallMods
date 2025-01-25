633deff302c7fd39960873ace6e5275883464ca7
bestsoft100 <163331759+Bestsoft101@users.noreply.github.com>
2025-01-25 08:27:52 +0100

    Did some coding

---
+++ src/client/java/b100/nonightvisionflicker/mixin/client/GameRendererMixin.java
@@ -0,0 +1,20 @@
+package b100.nonightvisionflicker.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.LivingEntity;
+
+@Mixin(value = GameRenderer.class)
+public class GameRendererMixin {
+	
+	@Inject(method = "getNightVisionStrength", at = @At("HEAD"), cancellable = true)
+	private static void overrideGetNightVisionStrength(LivingEntity entity, float tickDelta, CallbackInfoReturnable<Float> ci) {
+		ci.setReturnValue(1.0f);
+		ci.cancel();
+	}
+	
+}
