df07d0f70e928fdf762c66e469ed9cdd96f8a13c
bestsoft100 <163331759+Bestsoft101@users.noreply.github.com>
2025-04-30 00:06:30 +0200

    Check if player has night vision at all
    If the check isnt there Iris shaders thinks night vision is active at all times

--- src/client/java/b100/nonightvisionflicker/mixin/client/GameRendererMixin.java
+++ src/client/java/b100/nonightvisionflicker/mixin/client/GameRendererMixin.java
@@ -7,13 +7,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
 
 @Mixin(value = GameRenderer.class)
 public class GameRendererMixin {
 	
 	@Inject(method = "getNightVisionStrength", at = @At("HEAD"), cancellable = true)
 	private static void overrideGetNightVisionStrength(LivingEntity entity, float tickDelta, CallbackInfoReturnable<Float> ci) {
-		ci.setReturnValue(1.0f);
+		ci.setReturnValue(entity.hasStatusEffect(StatusEffects.NIGHT_VISION) ? 1.0f : 0.0f);
 		ci.cancel();
 	}
 	
